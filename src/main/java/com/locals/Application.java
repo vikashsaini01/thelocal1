c7b91675f3151e04799077b78b03ce6b813b16e6
Vikash Saini <vikashsaini01@gmail.com>
2016-11-23 12:51:20 +0530

    ,

--- src/main/java/com/locals/Application.java
+++ src/main/java/com/locals/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	//Comment
+	//Comment 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
